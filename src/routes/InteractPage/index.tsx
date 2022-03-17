666248b8872b9ecd1dcba359ce5b2330d374d6d2
Justin Dudley <justindudley99@gmail.com>
2022-03-16 23:58:37 -0400

    getting and displaying data from database

--- src/routes/InteractPage/index.tsx
+++ src/routes/InteractPage/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // import styles from './style.module.css';
 
 export const InteractPage: React.FC = () => {
+   const [tableau, setTableau] = useState({
+      document: { elaboration: { synopsis: '' } },
+   });
+   const dudmmyObj = { name: 'Justin' };
+
+   const fetchData = () => {
+      // need try-catch
+      fetch('http://localhost:8000/api/tableau/BH')
+         .then((res) => res.json())
+         .then((jsObj) => setTableau(jsObj));
+   };
+
    return (
       <>
          <div> Hello from INTERACTING page</div>
+         <div onClick={() => fetchData()}>click to fetch data</div>
+         <div>{tableau && tableau.document.elaboration.synopsis}</div>
          <nav>
             <Link to="/">back to HOME page</Link>
          </nav>

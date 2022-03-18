4a3d695f6b15aea772c92dc12f90966fd3d3c788
Justin Dudley <justindudley99@gmail.com>
2022-03-18 16:10:35 -0400

    move getTableau code to its own React component in its own file

---
+++ src/routes/InteractPage/PostComments/index.tsx
@@ -0,0 +1,3 @@
+import React, { useEffect } from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
--- src/routes/InteractPage/index.tsx
+++ src/routes/InteractPage/index.tsx
@@ -1,22 +1,16 @@
-import { useFormik } from 'formik';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { useFormik } from 'formik';
 import * as yup from 'yup';
+
 import { Tableau } from '../../models/Tableau';
+import { GetTableau } from './GetTableau';
 
 // import styles from './style.module.css';
 
 export const InteractPage: React.FC = () => {
    const [tableau, setTableau] = useState<Tableau | null>(null);
    const [letterPair, setLetterPair] = useState('AB');
-
-   const fetchTableau = (letterPair: string) => {
-      // need try-catch?
-      fetch(`http://localhost:8000/api/tableau/${letterPair}`)
-         .then((res) => res.json())
-         .then((tableau) => setTableau(tableau));
-   };
 
    const postComment = (comment: string) => {
       // need error handling, need to inform user of post success or failure
@@ -32,23 +26,6 @@ export const InteractPage: React.FC = () => {
          });
    };
 
-   const yupSchemaValidator1 = yup.object().shape({
-      letterPair: yup
-         .string()
-         .required('letter pair must be exactly 2 letters long')
-         .length(2, 'letter pair must be exactly 2 letters long'),
-   });
-
-   const formik1 = useFormik({
-      initialValues: { letterPair: letterPair },
-      validationSchema: yupSchemaValidator1,
-      onSubmit: (value) => {
-         const letterPairUpper = value.letterPair.toUpperCase();
-         setLetterPair(letterPairUpper);
-         fetchTableau(letterPairUpper);
-      },
-   });
-
    const yupSchemaValidator2 = yup.object().shape({
       comment: yup.string().max(1500, 'Maximum character limit exceeded'),
    });
@@ -62,10 +39,6 @@ export const InteractPage: React.FC = () => {
       },
    });
 
-   useEffect(() => {
-      fetchTableau('AB');
-   }, []);
-
    return (
       <div style={{ padding: '15px' }}>
          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -74,61 +47,13 @@ export const InteractPage: React.FC = () => {
                <Link to="/">back to HOME page</Link>
             </nav>
          </div>
-         <form
-            onSubmit={formik1.handleSubmit}
-            style={{ padding: '40px 0 20px' }}
-         >
-            <div>
-               <span>current letter-pair: </span>
-               <span style={{ color: 'green', fontWeight: 'bold' }}>
-                  {letterPair}
-               </span>
-            </div>
-            <label htmlFor="letterPair">Enter a new letter-pair: </label>
-            <input
-               id="letterPair"
-               name="letterPair"
-               type="text"
-               onChange={formik1.handleChange}
-               value={formik1.values.letterPair.toUpperCase()}
-               style={{ width: '50px', color: 'green', fontWeight: 'bold' }}
-            />
-            <button type="submit">Submit</button>
-            <div style={{ height: '1.5rem', color: 'red', fontSize: '0.8rem' }}>
-               {formik1.errors.letterPair}
-            </div>
-         </form>
-         <div style={{ border: '1px solid purple', padding: '10px' }}>
-            {/* first conditional allows program to compile, and MAYBE keeps app from breaking if no response from BE: */}
-            {tableau &&
-               // second conditionals keeps app from breaking if no response from BE:
-               tableau.document && (
-                  <div>
-                     <div style={{ marginBottom: '10px' }}>
-                        <span style={{ fontWeight: 'bold' }}>
-                           letter-pair:{' '}
-                        </span>
-                        <span>{tableau.document.letter_pair}</span>
-                     </div>
-                     <div style={{ marginBottom: '10px' }}>
-                        <span style={{ fontWeight: 'bold' }}>
-                           starter-sentence:{' '}
-                        </span>
-                        <span>{tableau.document.starter_sentence}</span>
-                     </div>
-                     <div style={{ marginBottom: '10px' }}>
-                        <span style={{ fontWeight: 'bold' }}>synopsis: </span>
-                        <span>{tableau.document.elaboration.synopsis}</span>
-                     </div>{' '}
-                     <div style={{ marginBottom: '10px' }}>
-                        <span style={{ fontWeight: 'bold' }}>backstory: </span>
-                        <span>
-                           {tableau.document.elaboration.backstory.story}
-                        </span>
-                     </div>
-                  </div>
-               )}
-         </div>
+         <GetTableau
+            tableau={tableau}
+            setTableau={setTableau}
+            letterPair={letterPair}
+            setLetterPair={setLetterPair}
+         />
+
          <div style={{ marginTop: '50px' }}>
             <form onSubmit={formik2.handleSubmit}>
                <div style={{ marginBottom: '20px' }}>

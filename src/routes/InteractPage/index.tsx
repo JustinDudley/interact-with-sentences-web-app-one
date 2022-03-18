57fa84c6ef8805b57bf95d90c8cf5512442d5cef
Justin Dudley <justindudley99@gmail.com>
2022-03-18 15:29:55 -0400

    user can now post comments. dont have good error handling or user feedback after post

--- src/routes/InteractPage/index.tsx
+++ src/routes/InteractPage/index.tsx
@@ -18,20 +18,47 @@ export const InteractPage: React.FC = () => {
          .then((tableau) => setTableau(tableau));
    };
 
-   const yupSchemaValidator = yup.object().shape({
+   const postComment = (comment: string) => {
+      // need error handling, need to inform user of post success or failure
+      fetch(`http://localhost:8000/api/tableau/comment/${letterPair}`, {
+         method: 'POST',
+         headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify({ comment: comment }),
+      })
+         .then((res) => res.json())
+         .then((resp) => {
+            // should be 1
+            alert(resp.mongoSuccessObj.modifiedCount);
+         });
+   };
+
+   const yupSchemaValidator1 = yup.object().shape({
       letterPair: yup
          .string()
          .required('letter pair must be exactly 2 letters long')
          .length(2, 'letter pair must be exactly 2 letters long'),
    });
 
-   const formik = useFormik({
+   const formik1 = useFormik({
       initialValues: { letterPair: letterPair },
-      validationSchema: yupSchemaValidator,
+      validationSchema: yupSchemaValidator1,
       onSubmit: (value) => {
          const letterPairUpper = value.letterPair.toUpperCase();
          setLetterPair(letterPairUpper);
          fetchTableau(letterPairUpper);
+      },
+   });
+
+   const yupSchemaValidator2 = yup.object().shape({
+      comment: yup.string().max(1500, 'Maximum character limit exceeded'),
+   });
+
+   const formik2 = useFormik({
+      initialValues: { comment: '' },
+      validationSchema: yupSchemaValidator2,
+      onSubmit: (value, { resetForm }) => {
+         postComment(value.comment);
+         resetForm();
       },
    });
 
@@ -48,7 +75,7 @@ export const InteractPage: React.FC = () => {
             </nav>
          </div>
          <form
-            onSubmit={formik.handleSubmit}
+            onSubmit={formik1.handleSubmit}
             style={{ padding: '40px 0 20px' }}
          >
             <div>
@@ -62,13 +89,13 @@ export const InteractPage: React.FC = () => {
                id="letterPair"
                name="letterPair"
                type="text"
-               onChange={formik.handleChange}
-               value={formik.values.letterPair.toUpperCase()}
+               onChange={formik1.handleChange}
+               value={formik1.values.letterPair.toUpperCase()}
                style={{ width: '50px', color: 'green', fontWeight: 'bold' }}
             />
             <button type="submit">Submit</button>
             <div style={{ height: '1.5rem', color: 'red', fontSize: '0.8rem' }}>
-               {formik.errors.letterPair}
+               {formik1.errors.letterPair}
             </div>
          </form>
          <div style={{ border: '1px solid purple', padding: '10px' }}>
@@ -101,6 +128,32 @@ export const InteractPage: React.FC = () => {
                      </div>
                   </div>
                )}
+         </div>
+         <div style={{ marginTop: '50px' }}>
+            <form onSubmit={formik2.handleSubmit}>
+               <div style={{ marginBottom: '20px' }}>
+                  If you would like to comment on this sentence, or suggest a
+                  new sentence, please write it in the box below. If you would
+                  like, you can include your email or other identifying tag
+                  within the comment
+               </div>
+
+               <label htmlFor="comment">{`Enter a comment for ${letterPair}  `}</label>
+               <input
+                  id="comment"
+                  name="comment"
+                  type="text"
+                  onChange={formik2.handleChange}
+                  value={formik2.values.comment}
+                  style={{ width: '150px', color: 'blue', fontWeight: 'bold' }}
+               />
+               <button type="submit">Submit</button>
+               <div
+                  style={{ height: '1.5rem', color: 'red', fontSize: '0.8rem' }}
+               >
+                  {formik2.errors.comment}
+               </div>
+            </form>
          </div>
       </div>
    );
